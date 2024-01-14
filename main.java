059b1ebf50292468eb587a0c5cffeba131c0d1cd
Tushar-Alec <petermoor822@gmail.com>
2024-01-15 03:12:00 +0530

    test1 stack

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class main {
+    public static void main(String[] args) {
+
+    Stack<String> stack = new Stack<String>();
+    stack.push("laptop");
+    stack.push("headphone");
+
+    System.out.println(stack);
+    }
+}
